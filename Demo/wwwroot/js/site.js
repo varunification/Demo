b4d5b362c6d82718d4e7cb842436ba31baea5b46
varunification <letter2varun@gmx.com>
2023-07-23 20:00:57 +0530

    relaced pdf package to jspdf

--- Demo/wwwroot/js/site.js
+++ Demo/wwwroot/js/site.js
@@ -1,4 +1,56 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+    document.getElementById('btnExportPDF').addEventListener('click', function() {
+        var doc = new jsPDF('portrait');
+        const tableElement = document.getElementById('mytable1');
+        const tableElement2 = document.getElementById('mytable3');
+        const tableElement3 = document.getElementById('mytable4');
+        const tableElement4 = document.getElementById('mytable5');
+        var img = new Image();
+        img.src = 'images/big_image.jpg'
+        var img2 = new Image();
+        img2.src = 'images/small_image.PNG'
+        // Generate the PDF with the table
+        doc.autoTable({
+            html: tableElement,
+            startY: 20, // Y position of the table
+            margin: { top: 20 }, // Margin from the top of the page
+            theme: 'grid', // Optional table style
+            headStyles: { fillColor: [200, 200, 200] }, // Optional table header style
+            tableWidth: '50'
+        });
+        doc.autoTable({
+            html: tableElement2,
+            startY: 40, // Y position of the table
+            margin: { top: 20 }, // Margin from the top of the page
+            theme: 'grid', // Optional table style
+            headStyles: { fillColor: [200, 200, 200] }, // Optional table header style
+            tableWidth: '50'
+        });
+
+
+        doc.addImage(img, 'JPEG', 30, 60, 150, 75); // (imageData, format, x, y, width, height)
+        doc.addImage(img2, 'PNG', 150,140, 50, 20); // (imageData, format, x, y, width, height)
+
+        doc.autoTable({
+            html: tableElement3,
+            startY: 170, // Y position of the table
+            margin: { top: 5 }, // Margin from the top of the page
+            theme: 'grid', // Optional table style
+            headStyles: { fillColor: [200, 200, 200] }, // Optional table header style
+            tableWidth: '50'
+        });
+
+        doc.autoTable({
+            html: tableElement4,
+            startY: 210, // Y position of the table
+            margin: { top: 5 }, // Margin from the top of the page
+            theme: 'grid', // Optional table style
+            headStyles: { fillColor: [200, 200, 200] }, // Optional table header style
+            tableWidth: '50'
+        });
+            // Save the PDF with a specific name
+            doc.save('exported_page.pdf');
+    });
+
+
